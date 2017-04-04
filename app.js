148554d59216f305f4acca6688669bcd637b83b5
Devin G <engy100x@gmail.com>
2017-04-04 15:57:28 -0700

    fixed syntax errors for if/else statements and reorganized whitespace to fit typical standard

--- app.js
+++ app.js
@@ -5,52 +5,51 @@ alert('Let\'s check your memory from introductions ^_^');
 
 
 var veteran = prompt('Did I serve in the Armed Forces?');
-  console.log('Veteran?', veteran);
+console.log('Veteran?', veteran);
 
  if( veteran.toLowerCase() == 'yes') {
   alert('Nice!');
- }
 
- else {
+ } else {
   alert('Ooops!  Maybe you\'ll get the next one');
  }
 
 var food = prompt('Would you believe my favorite fruit is watermelon?');
-  console.log('love watermelon', food);
+console.log('love watermelon', food);
 
   if(food.toLowerCase() == 'yes') {
     alert('omg I LOVE watermelon');
-  }
 
-  else {
+  } else {
     alert('incorrect');
   }
 
 var home = prompt('Do I live in Seattle?');
-  console.log('live in Seattle?', home);
+console.log('live in Seattle?', home);
 
   if(home.toLowerCase() == 'no') {
+    alert('Correct!  Too expensive!');
+
 } else{
     alert('Sorry, I actually live across the Puget Sound');
   }
 
 var wine = prompt('Am I a fan of wine?');
-  console.log('wine', wine);
+console.log('wine', wine);
 
+  if(wine.toLowerCase() == 'yes') {
     alert('YES!  I love wine! mmmm');
-  }
 
-  else {
+  }else {
     alert('Actually I am a wine-o, haha');
   }
 
 var age = prompt('Am I in my 30\'s?');
-  console.log('30s', age);
+console.log('30s', age);
 
   if(age.toLowerCase() == 'yes') {
     alert('yeahhh I am becoming old');
-  }
 
-  else {
+  } else {
     alert('awww thank you, unfortunely I am getting old');
   }
