c23adae5fd7efc7c7726d169cef4c8a987b999d0
Devin G <engy100x@gmail.com>
2017-04-04 14:08:27 -0700

    New project.  Created initial block of code for the questions.

---
+++ app.js
@@ -0,0 +1,38 @@
+'use strict';
+console.log("Heyyyy");
+
+alert('Let\'s check your memory from introductions ^_^');
+
+
+var veteran = prompt('Did I serve in the Armed Forces?');
+  console.log('Veteran?', veteran);
+
+ if( veteran.toLowerCase() == 'yes') {
+  alert('Nice!');
+ }
+ else {
+  alert('Ooops!  Maybe you\'ll get the next one');
+ }
+
+var food = prompt('Would you believe my favorite fruit is watermelon?');
+  console.log('favorite fruit', food);
+
+  if(food.toLowerCase() == 'yes') {
+    alert('omg I LOVE watermelon');
+  }
+
+  else {
+    alert('incorrect');
+  }
+
+var home = prompt('Do I live in Seattle?');
+  console.log('where do I live?', home);
+
+  if(home.toLowerCase() == 'no') {
+    alert('BINGO!');
+  }
+  else{
+    alert('Sorry, I actually live across the Puget Sound');
+  }
+
+var 
