2d6e6f26422112779a53579301c1e40acc16085a
Devin G <engy100x@gmail.com>
2017-04-05 14:26:37 -0700

    added in a loop, which runs however it wont run through all iterations when wrong answer is given

--- app.js
+++ app.js
@@ -1,55 +1,74 @@
-'use strict';
-console.log("Heyyyy");
+// 'use strict';
+// console.log("Heyyyy");
+//
+// alert('Let\'s check your memory from introductions ^_^');
+//
+//
+// var veteran = prompt('Did I serve in the Armed Forces?');
+// console.log('Veteran?', veteran);
+//
+//  if( veteran.toLowerCase() == 'yes') {
+//   alert('Nice!');
+//
+//  } else {
+//   alert('Ooops!  Maybe you\'ll get the next one');
+//  }
+//
+// var food = prompt('Would you believe my favorite fruit is watermelon?');
+// console.log('love watermelon', food);
+//
+//   if(food.toLowerCase() == 'yes') {
+//     alert('omg I LOVE watermelon');
+//
+//   } else {
+//     alert('incorrect');
+//   }
+//
+// var home = prompt('Do I live in Seattle?');
+// console.log('live in Seattle?', home);
+//
+//   if(home.toLowerCase() == 'no') {
+//     alert('Correct!  Too expensive!');
+//
+// } else{
+//     alert('Sorry, I actually live across the Puget Sound');
+//   }
+//
+// var wine = prompt('Am I a fan of wine?');
+// console.log('wine', wine);
+//
+//   if(wine.toLowerCase() == 'yes') {
+//     alert('YES!  I love wine! mmmm');
+//
+//   }else {
+//     alert('Actually I am a wine-o, haha');
+//   }
+//
+// var age = prompt('Am I in my 30\'s?');
+// console.log('30s', age);
+//
+//   if(age.toLowerCase() == 'yes') {
+//     alert('yeahhh I am becoming old');
+//
+//   } else {
+//     alert('awww thank you, unfortunely I am getting old');
+//   }
 
-alert('Let\'s check your memory from introductions ^_^');
 
+var myVehicle = 3;
+var answer = true;
+var guess;
 
-var veteran = prompt('Did I serve in the Armed Forces?');
-console.log('Veteran?', veteran);
+for (var attempts = 4; attempts > 0 && answer; attempts--) {
+  guess = prompt('How many vehicles do you think I own including motorcyles?');
+  guess = parseInt(guess);
 
- if( veteran.toLowerCase() == 'yes') {
-  alert('Nice!');
-
- } else {
-  alert('Ooops!  Maybe you\'ll get the next one');
- }
-
-var food = prompt('Would you believe my favorite fruit is watermelon?');
-console.log('love watermelon', food);
-
-  if(food.toLowerCase() == 'yes') {
-    alert('omg I LOVE watermelon');
-
+  if(guess === myVehicle) {
+    alert('Correct!');
+    answer = false
+  } else if (attempts = 1){
+    alert('Sorry you are out of chances');
   } else {
-    alert('incorrect');
+    alert('Not quite, try again');
   }
-
-var home = prompt('Do I live in Seattle?');
-console.log('live in Seattle?', home);
-
-  if(home.toLowerCase() == 'no') {
-    alert('Correct!  Too expensive!');
-
-} else{
-    alert('Sorry, I actually live across the Puget Sound');
-  }
-
-var wine = prompt('Am I a fan of wine?');
-console.log('wine', wine);
-
-  if(wine.toLowerCase() == 'yes') {
-    alert('YES!  I love wine! mmmm');
-
-  }else {
-    alert('Actually I am a wine-o, haha');
-  }
-
-var age = prompt('Am I in my 30\'s?');
-console.log('30s', age);
-
-  if(age.toLowerCase() == 'yes') {
-    alert('yeahhh I am becoming old');
-
-  } else {
-    alert('awww thank you, unfortunely I am getting old');
-  }
+}
